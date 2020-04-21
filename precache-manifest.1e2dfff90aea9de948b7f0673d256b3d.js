1f940453d96704bb434fe8de42b8030d62737f1c
Manolingam <manolingam1996@gmail.com>
2020-04-21 23:34:31 +0530

    Updates

---
+++ precache-manifest.1e2dfff90aea9de948b7f0673d256b3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb85a92fe19c2dd0bbd1cf862db4603",
+    "url": "/metagame-3box/index.html"
+  },
+  {
+    "revision": "516fce0c5d8e25b6b446",
+    "url": "/metagame-3box/static/css/main.420cd4e4.chunk.css"
+  },
+  {
+    "revision": "9b8a5a624bdb770ba589",
+    "url": "/metagame-3box/static/js/2.b9b255bd.chunk.js"
+  },
+  {
+    "revision": "50eed5d7d20ab40ba30229cff54f2c1d",
+    "url": "/metagame-3box/static/js/2.b9b255bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516fce0c5d8e25b6b446",
+    "url": "/metagame-3box/static/js/main.b4accc56.chunk.js"
+  },
+  {
+    "revision": "d9fe31d27f992dfd4438",
+    "url": "/metagame-3box/static/js/runtime-main.3c5939a3.js"
+  },
+  {
+    "revision": "f3f97018016dd123ab7a9d6f8aaecc04",
+    "url": "/metagame-3box/static/media/metagame.f3f97018.png"
+  }
+]);
